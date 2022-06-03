69f745275314335042a797a6cc08feec3611f7af
Hameshh <jossy21w@yahoo.com>
2022-06-03 15:08:14 -0700

    Task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 with commas and spaces.
+ * main - Prints a series of numbers with commas and spaces.
  * 
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success);
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 58; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		if (i != 57)
+
+		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
